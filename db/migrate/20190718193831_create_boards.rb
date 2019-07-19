6aa5389d20c8356326e7204c83978838911058ca
railway <doittherailway@gmail.com>
2019-07-19 10:26:22 -0700

    Finish migration changes for board table

--- db/migrate/20190718193831_create_boards.rb
+++ db/migrate/20190718193831_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
       t.string :name, null: false
-      t.string :manufacturer, null: false
+      t.integer :manufacturer_id, null: false
       t.array :category
       t.string :mount_style
       t.boolean :in_production
@@ -10,8 +10,11 @@ class CreateBoards < ActiveRecord::Migration[5.2]
       t.float :length
       t.float :width
       t.ingeger :num_kicktails
-      
+
       t.timestamps
     end
+
+    add_index :boards, :name
+    add_index :boards, :manufacturer_id
   end
 end
